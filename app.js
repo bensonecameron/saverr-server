fe1a768d8533b7399e45384701d273fa84d25bfc
Cameron Benson <66973535+bensonecameron@users.noreply.github.com>
2020-09-07 18:48:41 -0400

    master update

--- app.js
+++ app.js
@@ -1,29 +1,28 @@
 //! ENV
-require('dotenv').config();
-let bcrypt = require('bcryptjs');
+require("dotenv").config();
+let bcrypt = require("bcryptjs");
 
 //! EXPRESS
-const express = require('express');
+const express = require("express");
 const app = express();
 
-
 //! CONTROLLERS
-const collection = require('./controllers/collectioncontroller')
-const post = require('./controllers/postcontroller')
-const user = require('./controllers/userscontroller'); 
+const collection = require("./controllers/collectioncontroller");
+const post = require("./controllers/postcontroller");
+const user = require("./controllers/userscontroller");
 
 //! DATABASE
-const sequelize = require('./db'); 
+const sequelize = require("./db");
 sequelize.sync();
 // sequelize.sync({force: true})
-app.use(express.json());  
-app.use(require('./middleware/headers'));
+app.use(express.json());
+app.use(require("./middleware/headers"));
 
 //! ROUTES
-app.use('/collection', collection)
-app.use('/post', post)
-app.use('/user', user)
+app.use("/collection", collection);
+app.use("/post", post);
+app.use("/user", user);
 
 app.listen(3001, function () {
-  console.log("App is listening on port 3001");
-})
+  console.log(`**** App is listening on ${process.env.PORT} *********`);
+});
