8010f735c799741b37af2d69edce3e85ae14b0e9
Cameron Benson <66973535+bensonecameron@users.noreply.github.com>
2020-09-07 19:01:21 -0400

    more fixes

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const Sequelize = require("sequelize");
 
-const sequelize = new Sequelize(process.env.DATABAUSE_URL, {
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: "postgres",
 });
 
